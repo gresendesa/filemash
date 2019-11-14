bf8869a2a0e70ddbe1504f52e235de051c09323e
gresendesa <gresendesa@gmail.com>
2019-11-13 23:24:41 -0300

    file turned into an object

--- filemash.py
+++ filemash.py
@@ -13,168 +13,169 @@ import os
 import re
 import string
 
-CONNECTION_COM = '@>'
-CONNECTION_PATTERN = CONNECTION_COM + ' ( +)?["\'].*["\']'
-DEPENDENCY_BRANCH = []
 
-def file_exists(file_path):
-	"""
-		Checks if the especified file exists (Boolean)
-	"""
-	if os.path.exists(file_path):
-		return True
-	else:
-		return False
+class FileMasher:
+	def __init__(self, file_name):
+		self.file_name = file_name
 
-def get_root_dir():
-	"""
-		It gets a root directory from the program argument
-	"""
-	root_dir = ''
-	if len(sys.argv) > 1 and len(os.path.dirname(sys.argv[1])) > 0:
-		root_dir = os.path.dirname(sys.argv[1]) + '/'
-	return root_dir
+	CONNECTION_COM = '@>'
+	CONNECTION_PATTERN = CONNECTION_COM + ' ( +)?["\'].*["\']'
+	DEPENDENCY_BRANCH = []
 
+	def file_exists(self, file_path):
+		"""
+			Checks if the especified file exists (Boolean)
+		"""
+		if os.path.exists(file_path):
+			return True
+		else:
+			return False
 
-def is_connection(line):
-	"""
-		Checks if the passed text maches to a connection sintax (Boolean)
-	"""
-	if re.match('^.*' + CONNECTION_PATTERN, line):
-		return True
-	else:
-		return False
-
-def get_connection_param(connection):
-	"""
-		Gets a string connection and extracts its parameter and interprets it as a relative path
-	"""
-	return re.search(CONNECTION_PATTERN, re.sub(' +', ' ', connection.strip())).group(0).strip().replace('"', '').replace("'", '').split(CONNECTION_COM)[1].strip()
-
-def get_branch_path(deep = None):
-	"""
-		It gets a path and bind the defined root dir
-	"""
-	path = get_root_dir()
-	if deep == None:
-		deep = len(DEPENDENCY_BRANCH)
-	for i in range(0, deep):
-		if len(os.path.dirname(DEPENDENCY_BRANCH[i])) > 0:
-			path += os.path.dirname(DEPENDENCY_BRANCH[i]) + '/'
-	return path
-
-def get_basename(path):
-	"""
-		Works exactly how the name sugests ;)
-	"""
-	return os.path.basename(path)
+	def get_root_dir(self):
+		"""
+			It gets a root directory from the program argument
+		"""
+		root_dir = ''
+		if len(self.file_name) > 1 and len(os.path.dirname(self.file_name)) > 0:
+			root_dir = os.path.dirname(self.file_name) + '/'
+		return root_dir
 
 
-def append_branch_path(path, just_basename = True):
-	"""
-		It appends the brach 
-	"""
-	if just_basename:
-		return os.path.normpath(get_branch_path() + os.path.basename(path))
-	else:
-		return os.path.normpath(get_branch_path() + path)
+	def is_connection(self, line):
+		"""
+			Checks if the passed text maches to a connection sintax (Boolean)
+		"""
+		if re.match('^.*' + self.CONNECTION_PATTERN, line):
+			return True
+		else:
+			return False
+
+	def get_connection_param(self, connection):
+		"""
+			Gets a string connection and extracts its parameter and interprets it as a relative path
+		"""
+		return re.search(self.CONNECTION_PATTERN, re.sub(' +', ' ', connection.strip())).group(0).strip().replace('"', '').replace("'", '').split(self.CONNECTION_COM)[1].strip()
+
+	def get_branch_path(self, deep = None):
+		"""
+			It gets a path and bind the defined root dir
+		"""
+		path = self.get_root_dir()
+		if deep == None:
+			deep = len(self.DEPENDENCY_BRANCH)
+		for i in range(0, deep):
+			if len(os.path.dirname(self.DEPENDENCY_BRANCH[i])) > 0:
+				path += os.path.dirname(self.DEPENDENCY_BRANCH[i]) + '/'
+		return path
+
+	def get_basename(self, path):
+		"""
+			Works exactly how the name sugests ;)
+		"""
+		return os.path.basename(path)
+
+
+	def append_branch_path(self, path, just_basename = True):
+		"""
+			It appends the brach 
+		"""
+		if just_basename:
+			return os.path.normpath(self.get_branch_path() + os.path.basename(path))
+		else:
+			return os.path.normpath(self.get_branch_path() + path)
 
 
 
-def get_processed_line(line):
-	"""
-		Process a line. 
-		If the line is a connection it returns the linked file. 
-		Else it returns the same input.
-	"""
-	if is_connection(line):
-		if file_exists(append_branch_path(get_connection_param(line), False)) and not(is_connection_circular(line)):
-			return re.sub(CONNECTION_PATTERN, compose(get_connection_param(line)), line)
+	def get_processed_line(self, line):
+		"""
+			Process a line. 
+			If the line is a connection it returns the linked file. 
+			Else it returns the same input.
+		"""
+		if self.is_connection(line):
+			if self.file_exists(self.append_branch_path(self.get_connection_param(line), False)) and not(self.is_connection_circular(line)):
+				return re.sub(self.CONNECTION_PATTERN, self.compose(self.get_connection_param(line)), line)
+			else:
+				return None
+		else:
+			return line
+
+	def get_parent_file_path(self, connection):
+		"""
+			Returns the previous position of passed connection on tree dependency
+		"""
+		path_pos_connection = self.DEPENDENCY_BRANCH.index(self.get_connection_param(connection))
+		if path_pos_connection > 0:
+			return os.path.normpath(self.get_branch_path(path_pos_connection - 1) + self.get_basename(self.DEPENDENCY_BRANCH[path_pos_connection - 1]))
+		else:
+			return "the main input"
+
+	def show_log_error(self, file_name, line_number, message):
+		"""
+			Shows a message error in the default way
+		"""
+		print("Error: {} ({}, on line {})".format(message, os.path.normpath(file_name), line_number))
+
+	def handle_error(self, file_name, line_number, connection):
+		"""
+			Finds out the type error
+		"""
+		if self.is_connection_circular(connection):
+			self.show_log_error(file_name, line_number, "Circular connection to {} defined previously at {}".format(self.append_branch_path(self.get_connection_param(connection), False), self.get_parent_file_path(connection)))
+		if self.file_exists(self.append_branch_path(self.get_connection_param(connection), False)):
+			self.show_log_error(file_name, line_number, "Dependency not satisfied")
+		else:
+			self.show_log_error(file_name, line_number, "File \"{}\" not found".format(self.append_branch_path(self.get_connection_param(connection), False)))
+
+	def concat(self, str_a, str_b):
+		"""
+			Returns pair of string concatenation or returns none if any of them is None
+		"""
+		if str_a != None and str_b != None:
+			return str_a + str_b
+		else:
+			result = None
+		return result
+
+	def build_file(self, file_path):
+		"""
+			Brings together all linked files
+		"""
+		with open(self.append_branch_path(file_path), 'r') as file:
+			built_file = ''
+			line_counter = 0
+			for line in file:
+				line_counter += 1
+				built_file = self.concat(built_file, self.get_processed_line(line))
+				if built_file == None:
+					self.handle_error(self.append_branch_path(file_path), line_counter, line)
+					break
+		return built_file
+
+	def is_connection_circular(self, connection):
+		"""
+			It checks if a connection is already taken along the dependency branch. It avoids infinite loops.
+		"""
+		if self.get_connection_param(connection) in self.DEPENDENCY_BRANCH:
+			return True
+		else:
+			return False
+
+	def compose(self, file_path):
+		"""
+			It composes the set of files implementing registering of instances to avoid circular connections
+		"""
+		self.DEPENDENCY_BRANCH.append(file_path)
+		composing = self.build_file(file_path)
+		self.DEPENDENCY_BRANCH.pop()
+		if composing != None:
+			return composing.strip('\n')
 		else:
 			return None
-	else:
-		return line
 
-def get_parent_file_path(connection):
-	"""
-		Returns the previous position of passed connection on tree dependency
-	"""
-	path_pos_connection = DEPENDENCY_BRANCH.index(get_connection_param(connection))
-	if path_pos_connection > 0:
-		return os.path.normpath(get_branch_path(path_pos_connection - 1) + get_basename(DEPENDENCY_BRANCH[path_pos_connection - 1]))
-	else:
-		return "the main input"
+	def mash(self):
+		print(self.compose(os.path.basename(self.file_name)))
 
-def show_log_error(file_name, line_number, message):
-	"""
-		Shows a message error in the default way
-	"""
-	print("Error: {} ({}, on line {})".format(message, os.path.normpath(file_name), line_number))
 
-def handle_error(file_name, line_number, connection):
-	"""
-		Finds out the type error
-	"""
-	if is_connection_circular(connection):
-		show_log_error(file_name, line_number, "Circular connection to {} defined previously at {}".format(append_branch_path(get_connection_param(connection), False), get_parent_file_path(connection)))
-	if file_exists(append_branch_path(get_connection_param(connection), False)):
-		show_log_error(file_name, line_number, "Dependency not satisfied")
-	else:
-		show_log_error(file_name, line_number, "File \"{}\" not found".format(append_branch_path(get_connection_param(connection), False)))
-
-def concat(str_a, str_b):
-	"""
-		Returns pair of string concatenation or returns none if any of them is None
-	"""
-	if str_a != None and str_b != None:
-		return str_a + str_b
-	else:
-		result = None
-	return result
-
-def build_file(file_path):
-	"""
-		Brings together all linked files
-	"""
-	with open(append_branch_path(file_path), 'r') as file:
-		built_file = ''
-		line_counter = 0
-		for line in file:
-			line_counter += 1
-			built_file = concat(built_file, get_processed_line(line))
-			if built_file == None:
-				handle_error(append_branch_path(file_path), line_counter, line)
-				break
-	return built_file
-
-def is_connection_circular(connection):
-	"""
-		It checks if a connection is already taken along the dependency branch. It avoids infinite loops.
-	"""
-	if get_connection_param(connection) in DEPENDENCY_BRANCH:
-		return True
-	else:
-		return False
-
-def compose(file_path):
-	"""
-		It composes the set of files implementing registering of instances to avoid circular connections
-	"""
-	DEPENDENCY_BRANCH.append(file_path)
-	composing = build_file(file_path)
-	DEPENDENCY_BRANCH.pop()
-	if composing != None:
-		return composing.strip('\n')
-	else:
-		return None
-
-def main_controller():
-	"""
-		It handles input program
-	"""
-	if len(sys.argv) > 1 and file_exists(sys.argv[1]):
-		print(compose(os.path.basename(sys.argv[1])))
-	else:
-		print("Invalid argument")
-
-main_controller()
+f = FileMasher(file_name="sample/animals_and_fruits.txt")
+f.mash()
---
+++ run_sample.py
@@ -0,0 +1,4 @@
+from filemash import FileMasher
+
+f = FileMasher(file_name="sample/animals_and_fruits.txt")
+f.mash()
